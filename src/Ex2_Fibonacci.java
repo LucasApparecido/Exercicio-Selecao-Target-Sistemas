30a0003f399b4cdbbac4fab751d014940dd4466f
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2024-08-30 16:00:34 -0300

    Exercício 2 completo

--- src/Ex2_Fibonacci.java
+++ src/Ex2_Fibonacci.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Ex2_Fibonacci {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para verificar se pertence à sequência de Fibonacci: ");
+        int number = scanner.nextInt();
+
+        if (isFibonacci(number)) {
+            System.out.println(number + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println(number + " não pertence à sequência de Fibonacci.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isFibonacci(int number) {
+        if (number < 0) {
+            return false;
+        }
+
+        int firstValue = 0;
+        int secondValue = 1;
+
+        if (number == firstValue || number == secondValue) {
+            return true;
+        }
+
+        while (secondValue <= number) {
+            int temp = firstValue + secondValue;
+            firstValue = secondValue;
+            secondValue = temp;
+
+            if (secondValue == number) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
