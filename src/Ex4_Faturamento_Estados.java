efb1ca78e327451c6ca80842e548e81c62c355bf
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2024-08-30 16:56:33 -0300

    Exercicio 4 finalizado

--- src/Ex4_Faturamento_Estados.java
+++ src/Ex4_Faturamento_Estados.java
@@ -1,5 +1,20 @@
 public class Ex4_Faturamento_Estados {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double sp = 67836.43;
+        double rj = 36678.66;
+        double mg = 29229.88;
+        double es = 27165.48;
+        double others = 19849.53;
+
+        double totalRevenue = sp + rj + mg + es + others;
+
+        System.out.println("Percentual de representação de cada estado no faturamento total:");
+
+        System.out.printf("SP: %.2f%%\n", (sp / totalRevenue) * 100);
+        System.out.printf("RJ: %.2f%%\n", (rj / totalRevenue) * 100);
+        System.out.printf("MG: %.2f%%\n", (mg / totalRevenue) * 100);
+        System.out.printf("ES: %.2f%%\n", (es / totalRevenue) * 100);
+        System.out.printf("Outros: %.2f%%\n", (others / totalRevenue) * 100);
     }
 }
